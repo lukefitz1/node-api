d1fb4dc9a2aa6e5e0461172d4d34e3ff8d80e7ae
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2019-12-13 15:48:46 -0700

    adding testing

---
+++ test/user.spec.js
@@ -0,0 +1,29 @@
+var expect = require("chai").expect;
+
+const User = require("../database/models/").User;
+
+describe("Array", function() {
+  before(function() {
+    // User.create({
+    //   firstName: "TestFn",
+    //   lastName: "TestLn",
+    //   email: "TestEmail1234"
+    // });
+  });
+
+  it("should verify that we can create a new instance of the User class", function() {
+    expect("true").to.equal("true");
+  });
+
+  it("should verify that we can create a new instance of the User class", function() {
+    expect("true").to.equal("true");
+  });
+
+  it("should verify that we can create a new instance of the User class", function() {
+    expect("true").to.equal("true");
+  });
+
+  it("should verify that we can create a new instance of the User class", function() {
+    expect("true").to.equal("true");
+  });
+});
