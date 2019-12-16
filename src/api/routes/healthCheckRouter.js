d5fad5de80d925c2ac3ff6f69495839662036c8a
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2019-12-16 09:24:50 -0700

    testing out eb deployments

--- src/api/routes/healthCheckRouter.js
+++ src/api/routes/healthCheckRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/healthcheck", (req, res) => {
-  res.send({ status: "UP" });
+  res.send({ status: "UP1" });
 });
 
 module.exports = router;
