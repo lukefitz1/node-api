dca7feb7181746e4b692286e5f508b8faf852b6e
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2019-12-17 12:39:30 -0700

    dynamodb test

--- src/api/routes/healthCheckRouter.js
+++ src/api/routes/healthCheckRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/healthcheck", (req, res) => {
-  res.send({ status: "UP5" });
+  res.send({ status: "UP7" });
 });
 
 module.exports = router;
