4464f0c3b74a1c1824b254fdd82cd4e4323c2370
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2019-12-18 08:39:52 -0700

    trying to get the commit message again

--- src/api/routes/healthCheckRouter.js
+++ src/api/routes/healthCheckRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/healthcheck", (req, res) => {
-  res.send({ status: "UP1" });
+  res.send({ status: "UP2" });
 });
 
 module.exports = router;
